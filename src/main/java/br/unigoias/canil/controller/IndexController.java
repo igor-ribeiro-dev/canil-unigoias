7fb351e01eee8956e5be4aa453666535bd636149
Igor Ribeiro <igorribeiro4467@gmail.com>
2022-06-23 00:08:32 -0300

    Corrige redirecionamento para front-end.

--- src/main/java/br/unigoias/canil/controller/IndexController.java
+++ src/main/java/br/unigoias/canil/controller/IndexController.java
@@ -4,7 +4,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.servlet.view.RedirectView;
+import java.io.File;
+import java.net.URISyntaxException;
 import java.util.HashMap;
 
 @RestController
@@ -12,7 +14,24 @@ import java.util.HashMap;
 public class IndexController {
 
     @GetMapping
-    public ResponseEntity<HashMap<String, String>> index() {
+    public RedirectView index() throws URISyntaxException {
+        RedirectView redirectView = new RedirectView();
+
+        String indexFile = "src/main/resources/public/index.html";
+
+        File indexPage = new File(indexFile);
+
+        if(indexPage.exists()) {
+            redirectView.setUrl("index.html");
+        } else {
+            redirectView.setUrl("/health");
+        }
+
+        return redirectView;
+    }
+
+    @GetMapping("/health")
+    public ResponseEntity<HashMap<String, String>> health() {
         HashMap<String, String> status = new HashMap<>();
         status.put("status", "OK");
         status.put("version", "1.0");
